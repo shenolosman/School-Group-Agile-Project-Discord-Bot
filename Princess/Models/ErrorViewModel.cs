84e1162b942a12a9a05e8bfc2b2d5d0530876934
hannahansson <89410611+hannahansson@users.noreply.github.com>
2022-05-18 15:01:40 +0200

    MVC Template

---
+++ Princess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Princess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
