3871d73f5823305834e61670a4b220cb6bdc2774
Jakob Dennryd <jakob.dennryd@utb.ecutbildning.se>
2022-05-24 10:59:42 +0200

    First iteration of database models

---
+++ Princess/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace Princess.Models
+{
+    public class Student
+    {
+        //Insert the Discord ID as the student's ID
+        public ulong Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Response { get; set; }
+
+        public bool Presence { get; set; }
+
+        public string? ReasonAbsence { get; set; }
+
+        public Class? Class { get; set; }
+    }
+}
---
+++ Princess/Models/Teacher.cs
@@ -0,0 +1,11 @@
+﻿namespace Princess.Models
+{
+    public class Teacher
+    {
+        //Insert the Discord ID as the teacher's ID
+        public ulong Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Class>? Classes { get; set; }
+    }
+}
