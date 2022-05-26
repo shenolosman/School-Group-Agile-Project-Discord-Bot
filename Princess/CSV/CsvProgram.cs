8ea01e53e19baee2da14ad1bb8e8a17d09a87bca
ronni söderberg <ronni.soderberg@gmail.com>
2022-05-26 22:41:12 +0200

    Impoterar nugets skapade modeller csvprogram i sin egna mapp lägger till

---
+++ Princess/CSV/CsvProgram.cs
@@ -0,0 +1,23 @@
+﻿using Princess.Data;
+using Princess.Models;
+
+namespace Princess.CSV
+{
+    public class CsvProgram
+    {
+
+        private readonly PresenceDbContext _ctx;
+
+        public CsvProgram(PresenceDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public static void WriteToCsvFile()
+        {
+            var attendenceList = new List<ExportToCSV>()
+            {
+                new ExportToCSV{student = "Anna bengtsson", presence = true, registerTime=DateTime.UtcNow, teacher="Mr.Bjorn", theClass="win21" }
+            };
+        }
+    }
+}
