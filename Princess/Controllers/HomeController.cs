6d35595402f75300dce717fb97f70710496133ed
hannahansson <89410611+hannahansson@users.noreply.github.com>
2022-05-27 13:36:48 +0200

    Added pagination, worked with seed data for the layout

--- Princess/Controllers/HomeController.cs
+++ Princess/Controllers/HomeController.cs
@@ -1,21 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Princess.Models;
 using System.Diagnostics;
+using Princess.Services;
 
 namespace Princess.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly PresenceHandler _presenceHandler;
+        public HomeController(ILogger<HomeController> logger, PresenceHandler presenceHandler)
         {
             _logger = logger;
+            _presenceHandler = presenceHandler;
+        }
+        private async Task<AttendanceView> GetAttendanceList(int currentPage)
+        {
+            var maxRowsPerPage = 10;
+            var attendanceModel = new AttendanceView();
+
+            var getAttendanceList = await _presenceHandler.GetAttendanceList();
+
+            attendanceModel.AttendanceList = (from student in getAttendanceList select student)
+                .OrderBy(x => x.Student.Name)
+                .OrderByDescending(x => x.Lecture.Date)
+                .Skip((currentPage - 1) * maxRowsPerPage)
+                .Take(maxRowsPerPage)
+                .ToList();
+
+            var pageCount = (double)(getAttendanceList.Count / Convert.ToDecimal(maxRowsPerPage));
+            
+            attendanceModel.PageCount = (int)Math.Ceiling(pageCount); 
+            attendanceModel.CurrentPageIndex = currentPage; 
+            
+            return attendanceModel;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index() 
+         { 
+             return View(await GetAttendanceList(1));
+         }
+        [HttpPost]
+        public async Task<IActionResult> Index(int currentPageIndex)
         {
-            return View();
+            return View(await GetAttendanceList(currentPageIndex));
         }
 
         public IActionResult Privacy()
